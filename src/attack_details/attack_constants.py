71a2f65c9117caa6e90cc4858df9cbd566cb22b6
mahesh <kamepallimaheshbabu6007@gmail.com>
2026-03-01 20:08:20 -0600

    Added backdoor attack into the malicious client data
    Created the constants file
    
    Updated the train and test dataset code.

--- src/attack_details/attack_constants.py
+++ src/attack_details/attack_constants.py
@@ -3,7 +3,7 @@ NUM_CLIENTS = 15
 HEAD_CLIENT = "client_0"
 
 ## total num of the malicious clients
-NUM_MALICIOUS = 3
+NUM_MALICIOUS = 9
 MAX_FAKE_RATIO = 0.7
 
 ## label flip attack constant
@@ -16,6 +16,7 @@ SEED = 42
 ## image processing constants
 IMAGE_SIZE = (224, 224)
 TARGET_SIZE = (64, 64)
+TEST_SPLIT = 0.12
 
 ## data paths
 DATA_DIR = '/home/mahesh/Desktop/Dissertation/tf_fd_cancer_fakeclients_detection/data/Kidney Cancer'
